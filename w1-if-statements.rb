985a6e953f5f383e6eaf490608d435d60c2f0a0c
ro <kronew6@gmail.com>
2022-01-05 10:52:13 -0600

    codes second exercise in 33 seconds

--- w1-if-statements.rb
+++ w1-if-statements.rb
@@ -8,7 +8,14 @@ else
 end
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
-
+x = 10
+if x < 10
+  puts -1
+elsif x > 10
+  puts 1
+else
+  puts 0
+end
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
