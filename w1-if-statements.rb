6ff7d65c2b035bee12627f0f3c8865d4d6fed888
ro <kronew6@gmail.com>
2022-01-05 22:17:09 -0600

    removes all solutions for a clean slate

--- w1-if-statements.rb
+++ w1-if-statements.rb
@@ -1,98 +1,28 @@
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
-x = 10
 
-if x == 10
-  puts 0
-else
-  puts -1
-end
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
-x = 10
-if x < 10
-  puts -1
-elsif x > 10
-  puts 1
-else
-  puts 0
-end
+
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
-x = 1
-y = 2
-if x < 10 && y < 10
-  puts 1
-else
-  puts 0
-end
+
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
-x = 100
-if x > 9000
-  puts 1
-else
-  puts -1
-end
+
 
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
-x = 10
-if x > 29
-  puts -1
-elsif x < 10
-  puts 9
-elsif x < 20
-  puts 19
-elsif x < 30
-  puts 29
-end
+
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
-x = 100
-y= 50
-if x > 10 || y > 10
-  puts 100
-else
-  puts -100
-end
+
 
 # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
-x = 10
-if x < 0
-  puts 1776
-else
-  puts 1979
-end
+
 
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
-x = 100
-if x == 100
-  puts 100
-elsif x == 99
-  puts 99
-else
-  puts 0
-end
+
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
-x = 100
-y = 0
 
-if x < 0 && y > 0
-  puts 1
-else
-  puts 0
-end
 
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
-x = 20.5
-if x > 80
-  puts 5
-elsif x > 60
-  puts 4
-elsif x > 40
-  puts 3
-elsif x > 20
-  puts 2
-else
-  puts 1
-end
